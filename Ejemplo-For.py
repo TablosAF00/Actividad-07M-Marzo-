2a3f7f8b50156bea5f7b854d14dc6cd6f23d93fc
TablosAF00 <playstation2chipeada@gmail.com>
2025-03-07 17:20:28 -0600

    Create Ejemplo-For.py
    Descripción de un ejemplo con la función For

---
+++ Ejemplo-For.py
@@ -0,0 +1,12 @@
+#El buclee For a diferencia del while es que esta definido por un numero de iteraciones desde el comienzo
+#Por ejemplo en este bucle for la variable "i" se seguira imprimiendo hasta que se cumpla la condicion inicial
+for i in range(0, 5): #A su vez "i" seguira eincrementando su valor en 1 por cada ciclo
+    print(i)
+
+# Salida:
+# 0
+# 1
+# 2
+# 3
+# 4
+#Como resultado obtenemos que "i" se imprimio 5 veces
