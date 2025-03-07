b5fa597993176bc93d83d66a8cdc64cf3a41f787
TablosAF00 <playstation2chipeada@gmail.com>
2025-03-07 17:37:40 -0600

    Create Ejemplo-While.py
    Ejemplo del bucle while

---
+++ Ejemplo-While.py
@@ -0,0 +1,9 @@
+#El bucle while esta definido por una condicion que mientras no se cumpla seguira porduciendo iteraciones
+#aunque puede no ser asi y seguir produciendo iteraciones indefinidas
+x = 5 #establecemos un valor inicial 
+while x > 0: #determinamos una condicion donde while creara iteraciones mientras x no sea mayor a 0
+    x -=1 #mientras se crea un ciclo x disminute su valor inicial en 1 
+    print(x)
+
+# Salida: 4,3,2,1,0
+#como resultado tenemos impreso el valor de x en cada iteracion hasta que su valor llego a 0 cumpliendo la condicion 
